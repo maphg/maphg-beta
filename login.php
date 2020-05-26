88640ee637a9455c4d2ff3c97af81a3693658cea
mapgh <soporte@maphg.com>
2020-05-26 09:13:16 -0500

    Confirmación de Repositorio Git

--- login.php
+++ login.php
@@ -18,7 +18,7 @@
                     </div>
                     <div class="mb-4">
                         <label class="block text-gray-800 text-sm font-bold mb-2" for="username">
-                            Usuario:
+                            Usuario
                         </label>
                         <input
                             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -29,7 +29,7 @@
                     </div>
                     <div class="mb-6">
                         <label class="block text-gray-800 text-sm font-bold mb-2" for="password">
-                            Contraseña:
+                            Contraseña
                         </label>
                         <input
                             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
