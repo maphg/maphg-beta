a5f092536334f9d4bbcc7a5604430a8d6ab88a98
maphg <soporte@maphg.com>
2020-08-31 18:24:07 -0500

    Cambio de URL iframe lavanderia.php

--- lavanderia.php
+++ lavanderia.php
@@ -173,7 +173,7 @@
             document.getElementById("pestañaCamaras").classList.remove('bg-blue-200');
             document.getElementById("pestañaPendientes").classList.add('bg-blue-200');
 
-            document.getElementById("iframPendientes").src = 'http://maphg.com/beta/modalPendientes.php?idSeccion=11&tipoPendiente=MCS&idUsuario=' + idUsuario + '&idDestino=' + idDestino;
+            document.getElementById("iframPendientes").src = 'https://www.maphg.com/beta/modalPendientes.php?idSeccion=11&tipoPendiente=MCS&idUsuario=' + idUsuario + '&idDestino=' + idDestino;
         }
 
 
