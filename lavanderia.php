aa26dbff71174f89e5ce27b2cadcc3a6591c7ab6
maphg <soporte@maphg.com>
2020-08-31 20:49:59 -0500

    Mejoras para lavanderia.php

--- lavanderia.php
+++ lavanderia.php
@@ -31,13 +31,13 @@
     <!-- Page Content  -->
     <ul class="flex border-b p-2">
         <li class="-mb-px mr-1" onclick="graficos();">
-            <a id="pestañaGraficos" class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">Gráficos</a>
+            <a id="pestañaGraficos" class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">GRÁFICOS</a>
         </li>
         <li class="mr-1" onclick="camaras();">
-            <a id="pestañaCamaras" class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">Camaras</a>
+            <a id="pestañaCamaras" class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">CÁMARAS</a>
         </li>
         <li class="mr-1" onclick="pendientes();">
-            <a id="pestañaPendientes" class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">Pendientes</a>
+            <a id="pestañaPendientes" class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">PENDIENTES ZIL</a>
         </li>
     </ul>
 
