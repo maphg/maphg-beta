c4f2ade90ad4068977a727aa6cf867aca5431fdf
maphg <soporte@maphg.com>
2020-09-10 19:42:15 -0500

    Correcion 4

--- php/plannerCrudPHP.php
+++ php/plannerCrudPHP.php
@@ -7470,18 +7470,18 @@ if (isset($_POST['action'])) {
                             if (strpos($url, "jpg") || strpos($url, "jpeg") || strpos($url, "png")) {
                                 if (strpbrk($adjuntoURL, ' ')) {
                                     $dataImagen .= "
-                                        <a href=\"$adjuntoURL\" target=\"_blank\">
-                                        <div class=\"bg-local bg-cover bg-center w-20 h-20 rounded-md border-2 m-2 cursor-pointer op1\" style=\"background-image: url($adjuntoURL)\">
-                                        </div>
-                                        </a>
-                                    ";
-                                } else {
-                                    $dataImagen .= "
                                     <a href=\"$adjuntoURL\" target=\"_blank\">
                                         <div class=\"m-2 cursor-pointer overflow-hidden w-20 h-20 rounded-md op2\">
                                             <img src=\"$adjuntoURL\" class=\"w-full\" alt=\"\">
                                         </div>
                                     </a>
+                                    ";
+                                } else {
+                                    $dataImagen .= "
+                                        <a href=\"$adjuntoURL\" target=\"_blank\">
+                                        <div class=\"bg-local bg-cover bg-center w-20 h-20 rounded-md border-2 m-2 cursor-pointer op1\" style=\"background-image: url($adjuntoURL)\">
+                                        </div>
+                                        </a>
                                     ";
                                 }
 
