db652533b790bf1c93f76e7c0a166cb5e0ec1941
maphg <soporte@maphg.com>
2020-09-21 14:25:22 -0500

    Nueva tabla agregada

--- subida_datos_db.php
+++ subida_datos_db.php
@@ -32,6 +32,7 @@
                 <option value="t_subalmacenes_items_stock">Stock Subalmacenes (t_subalmacenes_items_stock)</option>
                 <option value="t_users">Cuentas Usuarios (t_users)</option>
                 <option value="t_colaboradores">Cuentas Colaboradores (t_colaboradores)</option>
+                <option value="t_equipos">Equipos Tabla Antigua (t_equipos)</option>
             </select>
             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
