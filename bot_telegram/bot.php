23550a8d1759234eb2c56d54d3bc213efe597593
maphg <eduardopool9310@gmail.com>
2020-10-11 15:02:04 -0500

    bot 8

--- bot_telegram/bot.php
+++ bot_telegram/bot.php
@@ -1,9 +1,9 @@
 <?php
+echo "Bot";
 $botToken = "1396322757:AAF5C0bcZxR8_mEEtm3BFEJGhgHvLcE3X_E";
 $website = "https://api.telegram.org/bot" . $botToken;
 $update = file_get_contents('php://input');
 $update = json_decode($update, true);
-echo "Bot";
 echo $update;
 $chatId = $update["message"]["chat"]["id"];
 $chatType = $update["message"]["chat"]["type"];
