98862448f3a1a8f739b25b419b687de0198e80e7
maphg <eduardopool9310@gmail.com>
2020-10-11 16:57:09 -0500

    Bots

--- bot_telegram/bot.php
+++ bot_telegram/bot.php
@@ -2,10 +2,11 @@
 $botToken = "1396322757:AAF5C0bcZxR8_mEEtm3BFEJGhgHvLcE3X_E";
 $website = "https://api.telegram.org/bot1396322757:AAF5C0bcZxR8_mEEtm3BFEJGhgHvLcE3X_E";
 $update = file_get_contents('php://input');
-$update = json_decode($update);
+$update = json_decode($update, true);
 $chatId = "989320528";
 $chatType = $update["message"]["chat"]["type"];
 $message = $update["message"]["text"];
+$url = "https://api.telegram.org/bot1396322757:AAF5C0bcZxR8_mEEtm3BFEJGhgHvLcE3X_E/sendMessage?chat_id=989320528&text=spoifsdffdsf";
 
 switch ($message) {
     case 'ayuda':
