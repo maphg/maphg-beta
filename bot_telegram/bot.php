d795a4e9014e7c4cb67aec29908af13d0fb72dbd
maphg <eduardopool9310@gmail.com>
2020-10-11 14:30:10 -0500

    Bot Telegram

--- bot_telegram/bot.php
+++ bot_telegram/bot.php
@@ -27,6 +27,6 @@ switch ($message) {
 
 function sendMessage($chatId, $response)
 {
-    $url = $GLOBALS[website] . '/sendMessage?chat_id=' . $chatId . '&parse_mode=HTML&text=' . urlencode($response);
+    $url = $GLOBALS["botApi"] . '/sendMessage?chat_id=' . $chatId . '&parse_mode=HTML&text=' . urlencode($response);
     file_get_contents($url);
 }
