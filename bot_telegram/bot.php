9eed3bb81aed99363d5b7525ec27e22566cdba9c
maphg <eduardopool9310@gmail.com>
2020-10-11 16:21:07 -0500

    Test Bot

--- bot_telegram/bot.php
+++ bot_telegram/bot.php
@@ -1,6 +1,6 @@
 <?php
 $botToken = "1396322757:AAF5C0bcZxR8_mEEtm3BFEJGhgHvLcE3X_E";
-$website = "https://api.telegram.org/bot" . $botToken;
+$website = "https://api.telegram.org/bot1396322757:AAF5C0bcZxR8_mEEtm3BFEJGhgHvLcE3X_E";
 $update = file_get_contents('php://input');
 $update = json_decode($update);
 $chatId = "989320528";
@@ -22,6 +22,7 @@ switch ($message) {
 
 function sendMessage($chatId, $response)
 {
-    $url = $GLOBALS["website"] . '/sendMessage?chat_id=' . $chatId . '&text=' . $response;
+    // $url = $GLOBALS["website"] . '/sendMessage?chat_id=' . $chatId . '&text=' . urlencode($response);
+    $url = "https://api.telegram.org/bot1396322757:AAF5C0bcZxR8_mEEtm3BFEJGhgHvLcE3X_E/sendMessage?chat_id=989320528&text=spoifsdffdsf";
     file_get_contents($url);
 }
