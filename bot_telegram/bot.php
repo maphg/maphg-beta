f06283311056af3b095ff190f95e6cd23562a9e2
maphg <eduardopool9310@gmail.com>
2020-10-11 14:52:42 -0500

    Bot 4

--- bot_telegram/bot.php
+++ bot_telegram/bot.php
@@ -1,9 +1,9 @@
 <?php
 $botToken = "1396322757:AAF5C0bcZxR8_mEEtm3BFEJGhgHvLcE3X_E";
-$botApi = "https://api.telegram.org/bot" . $botToken;
+$website = "https://api.telegram.org/bot" . $botToken;
 $update = file_get_contents('php://input');
 $update = json_decode($update, TRUE);
-
+echo json_encode($update);
 $chatId = $update["message"]["chat"]["id"];
 $chatType = $update["message"]["chat"]["type"];
 $message = $update["message"]["text"];
@@ -23,6 +23,6 @@ switch ($message) {
 
 function sendMessage($chatId, $response)
 {
-    $url = $GLOBALS["botApi"] . '/sendMessage?chat_id=' . $chatId . '&parse_mode=HTML&text=' . urlencode($response);
+    $url = $GLOBALS[website] . '/sendMessage?chat_id=' . $chatId . '&parse_mode=HTML&text=' . urlencode($response);
     file_get_contents($url);
 }
