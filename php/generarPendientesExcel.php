a54bccf66a1553402c953cb231ec8bf156600498
maphg <soporte@maphg.com>
2020-10-13 10:49:21 -0500

    Se agrego CODSAP para Exporta Pendientes

--- php/generarPendientesExcel.php
+++ php/generarPendientesExcel.php
@@ -19,19 +19,19 @@ if (isset($_GET['listaIdF']) and isset($_GET['listaIdT']) and isset($_GET['gener
         $filtroF = "AND t_mc.id IN(0)";
     }
 
-    //Correctivos Generales Fallas.
-    $queryF = "SELECT t_mc.id, t_mc.creado_por, c_destinos.destino, c_secciones.seccion, c_subsecciones.grupo, t_equipos.equipo, t_mc.actividad, t_colaboradores.nombre, t_colaboradores.apellido 
-    FROM t_mc 
-    INNER JOIN c_destinos ON t_mc.id_destino = c_destinos.id 
-    INNER JOIN c_secciones ON t_mc.id_seccion = c_secciones.id 
-    INNER JOIN c_subsecciones ON t_mc.id_subseccion = c_subsecciones.id 
-    LEFT JOIN t_equipos ON t_mc.id_equipo = t_equipos.id 
-    LEFT JOIN t_users ON t_mc.responsable = t_users.id 
-    INNER JOIN t_colaboradores ON t_users.id_colaborador = t_colaboradores.id 
-    WHERE t_mc.activo = 1 $filtroF
+    //FALLAS Generales
+    $queryF = "
+        SELECT t_mc.id, t_mc.status_material, t_mc.codsap, t_mc.creado_por, c_destinos.destino, c_secciones.seccion, c_subsecciones.grupo, t_equipos.equipo, t_mc.actividad, t_colaboradores.nombre, t_colaboradores.apellido 
+        FROM t_mc 
+        INNER JOIN c_destinos ON t_mc.id_destino = c_destinos.id 
+        INNER JOIN c_secciones ON t_mc.id_seccion = c_secciones.id 
+        INNER JOIN c_subsecciones ON t_mc.id_subseccion = c_subsecciones.id 
+        LEFT JOIN t_equipos ON t_mc.id_equipo = t_equipos.id 
+        LEFT JOIN t_users ON t_mc.responsable = t_users.id 
+        INNER JOIN t_colaboradores ON t_users.id_colaborador = t_colaboradores.id 
+        WHERE t_mc.activo = 1 $filtroF
     ";
     //Fin Tipo de Pendiente es MC ahora -> Fallas.
-
 
     //Inicio Tipo Tareas.
     if ($listaIdT != "") {
@@ -40,7 +40,8 @@ if (isset($_GET['listaIdF']) and isset($_GET['listaIdT']) and isset($_GET['gener
         $filtroT = "AND t_mp_np.id IN(0)";
     }
 
-    $queryT = "SELECT t_mp_np.id, t_mp_np.id_usuario, c_destinos.destino, c_secciones.seccion, c_subsecciones.grupo, t_equipos.equipo, t_mp_np.titulo,  t_mp_np.titulo, t_colaboradores.nombre, t_colaboradores.apellido
+    //TAREAS Generales
+    $queryT = "SELECT t_mp_np.id, t_mp_np.status_material, t_mc.codsap, t_mp_np.id_usuario, c_destinos.destino, c_secciones.seccion, c_subsecciones.grupo, t_equipos.equipo, t_mp_np.titulo,  t_mp_np.titulo, t_colaboradores.nombre, t_colaboradores.apellido
     FROM t_mp_np
     INNER JOIN c_destinos ON t_mp_np.id_destino = c_destinos.id 
     INNER JOIN t_equipos ON t_mp_np.id_equipo = t_equipos.id
@@ -70,7 +71,8 @@ if (isset($_GET['listaIdF']) and isset($_GET['listaIdT']) and isset($_GET['gener
         $objPHPExcel->getActiveSheet()->setCellValue('G1', 'Creado Por');
         $objPHPExcel->getActiveSheet()->setCellValue('H1', 'U. Comentario');
         $objPHPExcel->getActiveSheet()->setCellValue('I1', 'Comentario de:');
-        $objPHPExcel->getActiveSheet()->setCellValue('I1', 'Tipo Pendiente');
+        $objPHPExcel->getActiveSheet()->setCellValue('J1', 'Tipo Pendiente');
+        $objPHPExcel->getActiveSheet()->setCellValue('K1', 'Material');
 
         $fila = 2;
         while ($row = mysqli_fetch_array($resultF)) {
@@ -82,6 +84,16 @@ if (isset($_GET['listaIdF']) and isset($_GET['listaIdT']) and isset($_GET['gener
             $actividad = $row['actividad'];
             $responsable = $row['nombre'] . " " . $row['apellido'];
             $creadoPorF = $row['creado_por'];
+            $materialF = $row['status_material'];
+            $codsapF = $row['codsap'];
+
+            if ($materialF != "" and $materialF != "0") {
+                $materialF = "SI";
+                $codsapF = $codsap;
+            } else {
+                $materialF = "";
+                $codsapF = "";
+            }
 
             $queryComentario = "SELECT t_mc_comentarios.comentario, t_colaboradores.nombre, 
                 t_colaboradores.apellido
@@ -128,7 +140,9 @@ if (isset($_GET['listaIdF']) and isset($_GET['listaIdT']) and isset($_GET['gener
             $objPHPExcel->getActiveSheet()->setCellValue('G' . $fila, $nombreCreadoF);
             $objPHPExcel->getActiveSheet()->setCellValue('H' . $fila, $comentario);
             $objPHPExcel->getActiveSheet()->setCellValue('I' . $fila, $realizoComentario);
-            $objPHPExcel->getActiveSheet()->setCellValue('I' . $fila, 'Fallas');
+            $objPHPExcel->getActiveSheet()->setCellValue('J' . $fila, 'Fallas');
+            $objPHPExcel->getActiveSheet()->setCellValue('K' . $fila, $materialF);
+            $objPHPExcel->getActiveSheet()->setCellValue('L' . $fila, $codsapF);
 
             //Contador de Celdas
             $fila++;
@@ -143,6 +157,16 @@ if (isset($_GET['listaIdF']) and isset($_GET['listaIdT']) and isset($_GET['gener
             $titulo = $rowT['titulo'];
             $responsable = $rowT['nombre'] . " " . $rowT['apellido'];
             $creadoPorT = $rowT['id_usuario'];
+            $materialT = $row['status_material'];
+            $codsapT = $row['codsap'];
+
+            if ($materialT != "" and $materialT != "0") {
+                $materialT = "SI";
+                $codsapT = $codsap;
+            } else {
+                $materialT = "";
+                $codsapT = "";
+            }
 
             $queryComentarioT = "SELECT comentario.comentarios_mp_np, 
             t_colaboradores.nombre, t_colaboradores.apellido 
@@ -186,7 +210,9 @@ if (isset($_GET['listaIdF']) and isset($_GET['listaIdT']) and isset($_GET['gener
             $objPHPExcel->getActiveSheet()->setCellValue('G' . $fila, $nombreCreado);
             $objPHPExcel->getActiveSheet()->setCellValue('H' . $fila, $comentarioT);
             $objPHPExcel->getActiveSheet()->setCellValue('I' . $fila, $comentarioDeT);
-            $objPHPExcel->getActiveSheet()->setCellValue('I' . $fila, 'Tareas');
+            $objPHPExcel->getActiveSheet()->setCellValue('J' . $fila, 'Tareas');
+            $objPHPExcel->getActiveSheet()->setCellValue('K' . $fila, $materialT);
+            $objPHPExcel->getActiveSheet()->setCellValue('L' . $fila, $codsapT);
 
             //Contador de Celdas
             $fila++;
