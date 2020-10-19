6d1e0fcb8d93244a68acf0b657529326f573f2c5
maphg <eduardopool9310@gmail.com>
2020-10-19 16:38:05 -0500

    Scroll

--- planner-cols.php
+++ planner-cols.php
@@ -912,7 +912,7 @@
                 <div class="flex flex-col contenedor mx-auto scrollbar">
                     <div class="-my-2 py-2 overflow-x-auto  scrollbar">
                         <div class="align-middle inline-block min-w-full shadow-md overflow-auto sm:rounded-lg border-b border-gray-200 scrollbar" style="max-height: 80vh;">
-                            <table class="min-w-full divide-y divide-gray-200 table-fixed sortable">
+                            <table class="min- w-full divide-y divide-gray-200 table-fixed sortable">
                                 <thead>
                                     <tr class="cursor-pointer bg-white">
 
