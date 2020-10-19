9a6efb83f4cdb2ecbc469bbd879036b066e5cb66
maphg <eduardopool9310@gmail.com>
2020-10-19 15:54:13 -0500

    Bot

--- bot_telegram/bot.php
+++ bot_telegram/bot.php
@@ -6,11 +6,11 @@ $update = json_decode($update, true);
 $chatId = "989320528";
 $chatType = $update["message"]["chat"]["type"];
 $message = $update["message"]["text"];
-$url = "https://api.telegram.org/bot1396322757:AAF5C0bcZxR8_mEEtm3BFEJGhgHvLcE3X_E/sendMessage?chat_id=989320528&text=spoifsdffdsf";
+// $url = "https://api.telegram.org/bot1396322757:AAF5C0bcZxR8_mEEtm3BFEJGhgHvLcE3X_E/sendMessage?chat_id=989320528&text=spoifsdffdsf";
 
 switch ($message) {
     case '/ayuda':
-        $response = "Pendiente 1";
+        $response = "Bot MAPHG";
         sendMessageX($chatId, $response);
         break;
     case '/pendientes':
