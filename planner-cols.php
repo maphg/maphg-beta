d5d3654a7ce6ba69a770c8678314e42becd50552
maphg <eduardopool9310@gmail.com>
2020-10-30 12:03:02 -0500

    Correciones

--- planner-cols.php
+++ planner-cols.php
@@ -933,7 +933,7 @@
             </div>
 
             <!-- CONTENIDO PROYECTOS -->
-            <div id="contenidoProyectos" class="flex flex-col contenedor mx-auto overflow-x-auto  scrollbar scrollbar">
+            <div id="contenidoProyectos" class="flex flex-col mx-auto contenedor overflow-x-auto  scrollbar">
                 <div class="flex flex-col contenedor mx-auto scrollbar">
                     <div class="-my-2 py-2 overflow-x-auto  scrollbar">
                         <div class="align-middle inline-block min-w-full shadow-md overflow-auto sm:rounded-lg border-b border-gray-200 scrollbar" style="max-height: 80vh;">
@@ -991,7 +991,7 @@
             <!-- CONTENIDO PROYECTOS -->
 
             <!-- CONTENIDO GANTT -->
-            <div id="contenidoGantt" class="flex flex-col mx-auto overflow-x-auto  scrollbar hidden">
+            <div id="contenidoGantt" class="flex flex-col mx-auto contenedor overflow-x-auto scrollbar hidden">
                 <div class="mt-2 w-full  flex flex-row justify-center items-start font-semibold text-xs text-bluegray-500 cursor-pointer overflow-y-auto scrollbar bg-white sm:rounded-lg border-b border-gray-200" style="max-height: 80vh;">
                     <div class="w-full h-full text-xxs uppercase mt-5" id="chartdiv"></div>
                 </div>
