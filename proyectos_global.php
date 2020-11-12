2ed93cf4e9bf01d70df917d7296e5eec371b5034
maphg <eduardopool9310@gmail.com>
2020-11-12 17:27:56 -0500

    Correción

--- proyectos_global.php
+++ proyectos_global.php
@@ -12,7 +12,6 @@
     <link rel="stylesheet" href="css/modales.css">
     <link rel="stylesheet" href="css/alertify.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/daterangepicker.css">
     
     <style>
         #arrowtooltipActividadesGeneral,
