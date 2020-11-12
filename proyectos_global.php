c487b4c543ca6724fa7bb971cf29a26342dd1b57
maphg <eduardopool9310@gmail.com>
2020-11-12 18:02:19 -0500

    Correción Formato

--- proyectos_global.php
+++ proyectos_global.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="css/modales.css">
     <link rel="stylesheet" href="css/alertify.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css">
 
     <style>
         #arrowtooltipActividadesGeneral,
@@ -887,7 +887,6 @@
 <!-- JS OPCIONALES -->
 
 <!-- DATERANGEPICKER -->
-<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 <!-- DATERANGEPICKER -->
