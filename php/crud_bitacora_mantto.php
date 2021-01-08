6b0e3b4d76dbb5dfa70dfebf962f5fbeaf4033a6
maphg <eduardopool9310@gmail.com>
2021-01-08 18:54:15 -0500

    Correcion

--- php/crud_bitacora_mantto.php
+++ php/crud_bitacora_mantto.php
@@ -599,19 +599,19 @@ if (isset($_POST['action'])) {
 
             //En ZI admite solo: DEC(1) - AUTO(24) - ZIA(8) - ZIC(9) - ZIE(10) - ZIL(11) - ZHP(12).
             // La función LIKE solo funciona para correctivos. 
-            $zonaFiltroMC = "AND (t_mc.id_seccion IN(1, 24, 8, 9, 10, 11, 12) OR t_mc.zona LIKE '%ZI%' or t_mc.bitacora_zi = 1)";
+            $zonaFiltroMC = "AND (t_mc.id_seccion IN(1, 24, 8, 9, 10, 11, 12) OR t_mc.zona LIKE '%ZI%')";
             $zonaFiltroMP = "AND t_equipos.id_seccion IN(1, 24, 8, 9, 10, 11)";
             $zonaFiltroMPNP = "AND t_equipos.id_seccion IN(1, 24, 8, 9, 10, 11)";
             $zonaFiltro = "AND (reporte_status_proyecto.id_seccion IN(1, 24, 8, 9, 10, 11) OR t_proyectos.fase LIKE '%ZI%')";
         } elseif ($idDestino == 7 and $zona == "TRS") {
 
-            $zonaFiltroMC = "AND (t_mc.id_subseccion IN($TRS) OR t_mc.zona LIKE '%TRS%' or t_mc.bitacora_trs = 1)";
+            $zonaFiltroMC = "AND (t_mc.id_subseccion IN($TRS) OR t_mc.zona LIKE '%TRS%')";
             $zonaFiltroMP = "AND t_equipos.id_subseccion IN($TRS)";
             $zonaFiltroMPNP = "AND t_equipos.id_subseccion IN($TRS)";
             $zonaFiltro = "AND (reporte_status_proyecto.id_subseccion IN($TRS) OR t_proyectos.fase LIKE '%TRS%')";
         } elseif ($idDestino == 7 and $zona == "GP") {
 
-            $zonaFiltroMC = "AND (t_mc.id_subseccion IN($GP) OR t_mc.zona LIKE '%GP%' or t_mc.bitacora_gp = 1)";
+            $zonaFiltroMC = "AND (t_mc.id_subseccion IN($GP) OR t_mc.zona LIKE '%GP%')";
             $zonaFiltroMP = "AND t_equipos.id_subseccion IN($GP)";
             $zonaFiltroMPNP = "AND t_equipos.id_subseccion IN($GP)";
             $zonaFiltro = "AND (reporte_status_proyecto.id_subseccion IN($GP) OR t_proyectos.fase LIKE '%GP%')";
