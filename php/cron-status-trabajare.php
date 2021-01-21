e08a1896e6bfce7035f46971b6d0cab82a7e8215
maphg <eduardopool9310@gmail.com>
2021-01-21 12:56:51 -0500

    Se agrego notifiación telegram para restaurar status

--- php/cron-status-trabajare.php
+++ php/cron-status-trabajare.php
@@ -1,25 +1,33 @@
 <?php
 
-// Función para Inicializar el Status_Trabajare. en t_mc.
-//date_default_timezone_set('America/Cancun');
-// setlocale(LC_MONETARY, 'es_ES');
 include 'conexion.php';
-//$fecha_hoy = date('Y-m-d G:m:s');
-//$fecha_fin_horario = date('Y-m-d 16:00:00');
-
-
+$APIERROR = 'https://api.telegram.org/bot1396322757:AAF5C0bcZxR8_mEEtm3BFEJGhgHvLcE3X_E/sendMessage?chat_id=989320528&text=STATUS: ';
+$resp = "FALLO INCIDENCIAS";
 $query = "UPDATE t_mc SET status_trabajare = '' WHERE status_trabajare != ''";
 if ($result = mysqli_query($conn_2020, $query)) {
-	echo  "Horario Finalizado, el Status Trabajare se Inicializo. Enviado por: SOPORTE@MAPHG.COM";
-} else {
-	echo "Parametros No Esperados, para Inicializar el Status Finalizado.  Enviado por: SOPORTE@MAPHG.COM";
+	$resp = "T = Restaurado en Incidencias EQUIPOS";
 }
+file_get_contents($APIERROR . $resp);
 
-$query = "UPDATE t_mp_np SET status_trabajando = 0 WHERE status_trabajare = 1";
 
+$resp = "FALLO INCIDENCIAS GENERALES";
+$query = "UPDATE t_mp_np SET status_trabajando = 0 WHERE status_trabajando = 1";
 if ($result = mysqli_query($conn_2020, $query)) {
-	echo  "Horario Finalizado, el Status Trabajare se Inicializo. Enviado por: SOPORTE@MAPHG.COM";
-} else {
-	echo "Parametros No Esperados, para Inicializar el Status Finalizado.  Enviado por: SOPORTE@MAPHG.COM";
+	$resp = "T = Restaurado en Incidencias Generales";
 }
+file_get_contents($APIERROR . $resp);
+
+$resp = "FALLO PREVENTIVOS";
+$query = "UPDATE t_mp_planificacion_iniciada SET status_trabajando = 0 WHERE status_trabajando = 1";
+if ($result = mysqli_query($conn_2020, $query)) {
+	$resp = "T = Restaurado en PREVENTIVOS";
+}
+file_get_contents($APIERROR . $resp);
+
+$resp = "FALLO PLANES DE ACCIÓN";
+$query = "UPDATE t_proyectos_planaccion SET status_trabajando = 0 WHERE status_trabajando = 1";
+if ($result = mysqli_query($conn_2020, $query)) {
+	$resp = "T = Restaurado en PLANES DE ACCIÓN";
+}
+file_get_contents($APIERROR . $resp);
 // Fin de la función.
