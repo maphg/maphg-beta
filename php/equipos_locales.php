984807a89ab09a7ed9403d1d3e103d4013a4b217
maphg <eduardopool9310@gmail.com>
2021-01-28 16:53:59 -0500

    Mejoras

--- php/equipos_locales.php
+++ php/equipos_locales.php
@@ -827,7 +827,7 @@ if (isset($_GET['action'])) {
 
         $query = "SELECT id, equipo, local_equipo, status, jerarquia
         FROM t_equipos_america
-        WHERE id_equipo_principal = $idEquipo and jerarquia = 'SECUNDARIO' and activo = 1";
+        WHERE id_equipo_principal = $idEquipo and jerarquia = 'SECUNDARIO' and activo = 1 and status IN('OPERATIVO', 'TALLER')";
         if ($resultEquipo = mysqli_query($conn_2020, $query)) {
             foreach ($resultEquipo as $x) {
                 $idEquipo = $x['id'];
