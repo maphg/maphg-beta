f2abd24c3911744c9b46e1177740940579b50558
maphg <eduardopool9310@gmail.com>
2021-03-24 19:28:16 -0500

    Correción

--- gestion_equipos/php/gestion_equipos_crud.php
+++ gestion_equipos/php/gestion_equipos_crud.php
@@ -73,7 +73,7 @@ if (isset($_GET['action'])) {
         LEFT JOIN c_marcas ON t_equipos_america.id_marca = c_marcas.id
         LEFT JOIN c_ubicaciones ON t_equipos_america.id_ubicacion = c_ubicaciones.id
         INNER JOIN c_destinos ON t_equipos_america.id_destino = c_destinos.id
-        WHERE t_equipos_america.activo = 1 and t_equipos_america.status IN('OPERATIVO', 'TALLER')
+        WHERE t_equipos_america.activo = 1 and t_equipos_america.status IN('OPERATIVO', 'TALLER', 'FUERASERVICIO', 'OPERAMAL')
         $filtroDestino $filtroSeccion  $filtroSubseccion $filtroTipo $filtroStatus $filtroPalabra";
         if ($result = mysqli_query($conn_2020, $query)) {
             foreach ($result as $i) {
