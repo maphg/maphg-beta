9ac484663c08e41dbddbe967548e25e35f81350b
Eduardo <eduardopool9310@gmail.com>
2021-04-01 12:23:45 -0500

    Mejoras

--- php/api-reportes-incidencias.php
+++ php/api-reportes-incidencias.php
@@ -27,6 +27,9 @@ if (isset($_GET['action'])) {
             $filtroDestino = "and c_destinos.id = $idDestino";
         }
 
+        #PERIODO
+        // $array['PERIODO'] = $fechaInicio . " - " . $fechaFin;
+
         $query = "SELECT c_destinos.id 'idDestino', c_destinos.destino, c_secciones.id 'idSeccion', c_secciones.seccion
         FROM c_destinos
         INNER JOIN c_rel_destino_seccion ON c_destinos.id = c_rel_destino_seccion.id_destino
@@ -49,6 +52,8 @@ if (isset($_GET['action'])) {
                 $mediaPendientes = 0;
                 $mediaSolucionados = 0;
                 $totalComentarios = 0;
+                $horasPentientesGlobal = 0;
+                $horasSolucionadosGlobal = 0;
                 $resultado = array();
 
                 $query = "SELECT id, actividad, fecha_creacion, fecha_realizado, status FROM t_mc WHERE id_destino = $idDestino and id_seccion = $idSeccion and activo = 1 and status IN('PENDIENTE', 'N', 'SOLUCIONADO', 'F') and ((fecha_creacion BETWEEN '$fechaInicio' and '$fechaFin') OR 
@@ -58,12 +63,16 @@ if (isset($_GET['action'])) {
                         $totalIncidencias++;
                         $idActividad = $x['id'];
                         $actividad = $x['actividad'];
-                        $fechaCreacion = $x['fecha_creacion'];
+                        $fechaCreacion = (new DateTime($x['fecha_creacion']))->format('Y-m-d H:m:s');
                         $fechaRealizado = $x['fecha_realizado'];
                         $status = $x['status'];
+                        $tiempoPendiente = 0;
+                        $tiempoSolucionado = 0;
 
-                        $horaCreacion = (new DateTime($fechaCreacion))->format('H:m:s');
-                        $horaFin = (new DateTime($fechaFin))->format('H:m:s');
+                        #OBTIENE TIEMPOS EN HORAS
+                        $horasCreacion = strtotime($fechaCreacion);
+                        $horasSolucionado = strtotime($fechaRealizado);
+                        $horasActual = strtotime($fechaActual);
 
                         $query = "SELECT count(id) 'total' FROM t_mc_comentarios 
                         WHERE id_mc = $idActividad and activo = 1";
@@ -75,37 +84,57 @@ if (isset($_GET['action'])) {
 
                         if ($status == "PENDIENTE" || $status == "N") {
                             $pendientes++;
-                            $mediaPendientes = $horaCreacion . $horaFin;
-                            $status = "PENDIENTE";
+
+                            if ($horasActual > 0 && $horasCreacion > 0) {
+                                $tiempoPendiente = ($horasActual - $horasCreacion) / 3600;
+                                $horasPentientesGlobal += $tiempoPendiente;
+                            }
+
+
                             $resultado['PENDIENTE'][] = array(
                                 "OT Incidencia" => intval($idActividad),
                                 "Incidencia" => $actividad,
                                 "Fecha Creacion" => $fechaCreacion,
+                                "Fecha Actual" => $fechaActual,
                                 "Fecha Finalizado" => $fechaRealizado,
-                                "Status" => $status
+                                "Tiempo Pendiente" => number_format($tiempoPendiente, 2, '.', '') . " Hora(s)",
+                                "Status" => "PENDIENTE"
                             );
                         } else {
                             $solucionados++;
-                            $horaRealizado = (new DateTime($fechaRealizado))->format('H:m:s');
-                            $mediaSolucionados = $horaCreacion . $horaRealizado;
-                            $status = "SOLUCIONADO";
+
+                            if ($horasCreacion > 0 && $horasSolucionado > 0) {
+                                $tiempoSolucionado = ($horasSolucionado - $horasCreacion) / 3600;
+                                $horasSolucionadosGlobal += $tiempoSolucionado;
+                            }
+
                             $resultado['SOLUCIONADO'][] = array(
                                 "OT Incidencia" => intval($idActividad),
                                 "Incidencia" => $actividad,
                                 "Fecha Creacion" => $fechaCreacion,
                                 "Fecha Finalizado" => $fechaRealizado,
-                                "Status" => $status
+                                "Tiempo Solucionado" => number_format($tiempoSolucionado, 2, '.', '') . " Hora(s)",
+                                "Status" => "SOLUCIONADO"
                             );
                         }
                     }
                 }
 
+                #MEDIA DE PENDIENTES POR HORAS
+                if ($horasPentientesGlobal > 0 && $pendientes > 0) {
+                    $mediaPendientes = $horasPentientesGlobal / $pendientes;
+                }
+
+                #MEDIA DE SOLUCIONADOS POR HORAS_SOLUCIONADO
+                if ($horasSolucionadosGlobal > 0 && $solucionados > 0) {
+                    $mediaSolucionados = $horasSolucionadosGlobal / $solucionados;
+                }
                 $array[$destino][$seccion][] = array(
                     "TOTAL INCIDENCIAS" => intval($totalIncidencias),
                     "TOTAL INCIDENCIAS PENDIENTES" => intval($pendientes),
                     "TOTAL INCIDENCIAS SOLUCIONADOS" => intval($solucionados),
-                    "MEDIA TIEMPO PENDIENTES" => $mediaPendientes,
-                    "MEDIA TIEMPO SOLUCIONADOS" => 0,
+                    "MEDIA TIEMPO PENDIENTES" => number_format($mediaPendientes, 2, '.', '') . " Hora(s)",
+                    "MEDIA TIEMPO SOLUCIONADOS" => number_format($mediaSolucionados, 2, '.', '') . " Hora(s)",
                     "TOTAL COMENTARIOS" => $totalComentarios,
                     "RESUMEN: " => $resultado
                 );
