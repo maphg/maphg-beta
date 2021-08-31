b32b76e8409cb336e9e1303f6ee92623c4e1faff
Eduardo <eduardopool9310@gmail.com>
2021-08-31 00:28:22 -0500

    Mejoras

--- apis/sabanas.php
+++ apis/sabanas.php
@@ -3,7 +3,7 @@
 date_default_timezone_set('America/Cancun');
 setlocale(LC_MONETARY, 'es_ES');
 $fechaActual = date('Y-m-d H:m:s');
-
+$caracteres = array("[", "]", "[ ", "] ");
 
 # CABECERA PARA JSON
 header("Access-Control-Allow-Origin: *");
@@ -427,7 +427,10 @@ if ($peticion === "POST") {
          $filtroDestinoEquipo = "and e.id_destino = $idDestino";
       }
 
-
+      if ($idSabana == "TODOS")
+         $filtroSabana = "";
+      else
+         $filtroSabana = "and id_sabana = '$idSabana'";
 
       // SABANAS
       $query = "SELECT id 'idHotel', hotel
@@ -438,8 +441,9 @@ if ($peticion === "POST") {
             $idHotel = $x['idHotel'];
             $hotel = $x['hotel'];
 
+            $totalRegistros = 0;
             $equipos = array();
-            $query = "SELECT id 'idEquipo', equipo
+            $query = "SELECT id_equipo 'idEquipo', equipo
             FROM t_sabanas_equipos
             WHERE activo = 1 $filtroDestino";
             if ($result = mysqli_query($conn_2020, $query)) {
@@ -450,13 +454,15 @@ if ($peticion === "POST") {
                   $registros = array();
                   $query = "SELECT id_publico 'idRegistro', semana, fecha_creado, creado_por
                   FROM t_sabanas_registros
-                  WHERE id_equipo = $idEquipo and activo = 1";
+                  WHERE id_equipo ='$idEquipo' and activo = 1 and
+                  fecha_creado BETWEEN '$fechaInicial 00:00:01' and '$fechaFinal 23:59:59' $filtroSabana";
                   if ($result = mysqli_query($conn_2020, $query)) {
                      foreach ($result as $x) {
                         $idRegistro = $x['idRegistro'];
                         $semana = $x['semana'];
                         $fecha = $x['fecha_creado'];
                         $idUsuarioX = $x['creado_por'];
+                        $totalRegistros++;
 
                         $creadoPor = "";
                         $query = "SELECT u.id, c.nombre, c.apellido
@@ -486,11 +492,22 @@ if ($peticion === "POST") {
                }
             }
 
-            $array['data'][] = array(
-               "idHotel" => $idHotel,
-               "hotel" => $hotel,
-               "equipos" => $equipos,
-            );
+            // SOLO CON DATOS
+            if ($visualizar == 1)
+               if ($totalRegistros > 0)
+                  $array['data'][] = array(
+                     "idHotel" => $idHotel,
+                     "hotel" => $hotel,
+                     "equipos" => $equipos,
+                  );
+
+            // INCLUYE DATOS VACIOS
+            if ($visualizar == 0)
+               $array['data'][] = array(
+                  "idHotel" => $idHotel,
+                  "hotel" => $hotel,
+                  "equipos" => $equipos,
+               );
          }
       }
 
@@ -523,7 +540,6 @@ if ($peticion === "POST") {
 
       $query = "SELECT id_publico, sabana FROM t_sabanas
       WHERE activo = 1 and id_hotel = '$idHotel' $filtroDestino";
-      $array['query'] = $query;
       if ($result = mysqli_query($conn_2020, $query)) {
          foreach ($result as $x) {
             $idSabana = $x['id_publico'];
@@ -532,6 +548,120 @@ if ($peticion === "POST") {
             $array['data'][] = array(
                "idSabana" => $idSabana,
                "sabana" => $sabana,
+            );
+         }
+      }
+   }
+
+   if ($action === "detallesActividad") {
+      $array['response'] = "SUCCESS";
+      $idRegistro = $_POST['idRegistro'];
+
+      $query = "SELECT s.id_publico, s.sabana, c.nombre, c.apellido,
+      r.fecha_creado, r.ids_apartados, r.ids_actividades
+      FROM t_sabanas_registros AS r
+      INNER JOIN t_sabanas AS s ON r.id_sabana = s.id_publico
+      INNER JOIN t_users AS u ON r.creado_por = u.id
+      INNER JOIN t_colaboradores AS c ON u.id_colaborador = c.id
+      WHERE r.id_publico = '$idRegistro'";
+      if ($result = mysqli_query($conn_2020, $query)) {
+         foreach ($result as $x) {
+            $idSabana  = $x['id_publico'];
+            $sabana = $x['sabana'];
+            $creadoPor = $x['nombre'] . " " . $x['apellido'];
+            $fechaCreado = $x['fecha_creado'];
+
+            #APARTADOS
+            $idsApartados = str_replace($caracteres, "", $x['ids_apartados']);
+            $idsApartados = explode(", ", $idsApartados);
+
+            #ACTIVIDADES
+            $idsActividades = str_replace($caracteres, "", $x['ids_actividades']);
+            $idsActividades = explode(", ", $idsActividades);
+            $idsActividadesX = array();
+            foreach ($idsActividades as $x) {
+               $idsActividadesX[] = $x;
+            }
+            $idsActividadesX = implode("','", $idsActividadesX);
+
+            $arrayApartados = array();
+            foreach ($idsApartados as $x) {
+               $idApartado = $x;
+               $query = "SELECT id_publico, apartado FROM t_sabanas_apartados
+               WHERE id_publico = '$idApartado'";
+               if ($result = mysqli_query($conn_2020, $query)) {
+                  foreach ($result as $x) {
+                     $idApartadoX = $x['id_publico'];
+                     $apartado = $x['apartado'];
+
+                     $arrayActividades = array();
+                     $query = "SELECT id_publico, actividad FROM t_sabanas_apartados_actividades
+                     WHERE id_apartado = '$idApartadoX' and id_publico IN('$idsActividadesX')";
+                     if ($result = mysqli_query($conn_2020, $query)) {
+                        foreach ($result as $x) {
+                           $idActividad = $x['id_publico'];
+                           $actividad = $x['actividad'];
+
+                           $idCaptura = "";
+                           $tieneComentario = "NO";
+                           $comentario = "";
+                           $tieneFoto = "NO";
+                           $foto = "";
+                           $valor = "0";
+                           $query = "SELECT id_publico, valor, comentario, url_adjunto
+                           FROM t_sabanas_registros_capturas
+                           WHERE id_registro = '$idRegistro' and id_actividad = '$idActividad'";
+                           if ($result = mysqli_query($conn_2020, $query)) {
+                              foreach ($result as $x) {
+                                 $idCaptura = $x['id_publico'];
+                                 $valor = $x['valor'];
+                                 $comentario = $x['comentario'];
+                                 $foto = $x['url_adjunto'];
+
+                                 if ($comentario != "")
+                                    $tieneComentario = "SI";
+
+                                 if ($foto != "")
+                                    $tieneFoto = "SI";
+
+                                 if ($valor === "SI")
+                                    $valor = 1;
+                                 elseif ($valor === "NO")
+                                    $valor = 2;
+                                 elseif ($valor === "N/A")
+                                    $valor = 3;
+                              }
+                           }
+
+                           $arrayActividades[] = array(
+                              "idActividad" => $idActividad,
+                              "actividad" => $actividad,
+                              "idCaptura" => $idCaptura,
+                              "tieneComentario" => $tieneComentario,
+                              "comentario" => $comentario,
+                              "tieneFoto" => $tieneFoto,
+                              "foto" => $foto,
+                              "valor" => $valor,
+                           );
+                        }
+                     }
+
+                     $arrayApartados[] = array(
+                        "idApartado" => $idApartadoX,
+                        "apartado" => $apartado,
+                        "actividades" => $arrayActividades,
+                     );
+                  }
+               }
+            }
+
+            $array['data'][] = array(
+               "idSabana" => $idSabana,
+               "sabana" => $sabana,
+               "creadoPor" => $creadoPor,
+               "fechaCreado" => $fechaCreado,
+               "idsApartados" => $idsApartados,
+               "apartados" => $arrayApartados,
             );
          }
       }
